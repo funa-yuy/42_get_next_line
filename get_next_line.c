7a8f54083134aaf5e74fb01f02aff7ad0537e242
funa <orz.home35@gmail.com>
2024-09-08 02:04:18 +0900

    🚧 Norminette get_next_line.c get_next_line.h is OK!

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 20:42:36 by miyuu             #+#    #+#             */
-/*   Updated: 2024/09/07 19:29:23 by miyuu            ###   ########.fr       */
+/*   Updated: 2024/09/08 02:03:27 by miyuu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,20 @@ static char	*nullize_free(char *p)
 	return (NULL);
 }
 
-
 static void	input_line(char *lines, char *buf, size_t read_byte)
 {
 	char	*memo;
 
 	if (lines == NULL)
-		lines = ft_substr(buf, 0, read_byte);//linesに入れる
+		lines = ft_substr(buf, 0, read_byte);
 	else
 	{
 		memo = lines;
-		lines = ft_strjoin(lines, buf); //ft_strjoinで、linesと bufを連結
+		lines = ft_strjoin(lines, buf);
 		free(memo);
 	}
 	if (lines == NULL)
 		nullize_free(lines);
-
 }
 
 static char	*extract_line(char *lines)
@@ -44,21 +42,20 @@ static char	*extract_line(char *lines)
 	char	*origin;
 	char	*one_line;
 
-	line_n = ft_strchr(lines, '\n');//最初の改行以降の文字列
-	line_end = lines + ft_strlen(lines);//linesの先頭ポインターに、linesの文字数を足す→linesの末尾を指すポインタを意味する
-	origin = lines;//元の文字列も先頭ポインター
+	line_n = ft_strchr(lines, '\n');
+	line_end = lines + ft_strlen(lines);
+	origin = lines;
 	lines = NULL;
-	if (line_n == NULL || line_n + 1 == line_end)//改行がないか、改行が最後の文字だったら
+	if (line_n == NULL || line_n + 1 == line_end)
 		one_line = ft_substr(origin, 0, line_end - origin);
 	else
 	{
-		one_line = ft_substr(origin, 0, line_n - origin + 1);//元の文字列の0文字目から、改行まで
-		lines = ft_substr(origin, line_n - origin + 1, line_end - line_n);//改行以降の文字列
+		one_line = ft_substr(origin, 0, line_n - origin + 1);
+		lines = ft_substr(origin, line_n - origin + 1, line_end - line_n);
 		if (one_line == NULL || lines == NULL)
 		{
 			nullize_free(one_line);
 			nullize_free(lines);
-
 		}
 	}
 	free(origin);
@@ -75,126 +72,37 @@ char	*get_next_line(int fd)
 		return (NULL);
 	while (lines == NULL || ft_strchr(lines, '\n') == NULL)
 	{
-		read_byte = read(fd, &buf, BUFFER_SIZE); //BUFFER_SIZE分リード
+		read_byte = read(fd, &buf, BUFFER_SIZE);
 		if (read_byte <= 0)
 			nullize_free(lines);
 		if (read_byte == 0 && lines == NULL)
 			nullize_free(lines);
-		buf[read_byte] = '\0';//最後に終端ヌル
+		buf[read_byte] = '\0';
 		input_line(lines, buf, read_byte);
-		// if (lines == NULL)
-		// 	lines = ft_substr(buf, 0, read_byte);//linesに入れる
-		// else
-		// {
-		// 	memo = lines;
-		// 	lines = ft_strjoin(lines, buf); //ft_strjoinで、linesと bufを連結
-		// 	free(memo);
-		// }
-		// if (lines == NULL)
-		// 	nullize_free(&lines);
-		if (read_byte < BUFFER_SIZE)//１行の長さがバッファサイズより大きかったらwhileを回す
+		if (read_byte < BUFFER_SIZE)
 			break ;
 	}
 	return (extract_line(lines));
 }
 
-int	main(void)
-{
-	int		fd;
-	char	*one_line;
-
-	fd = open("./text.txt", O_RDONLY);
-	if (fd == -1)
-	{
-		perror("open");
-		return (1);
-	}
-	while ((one_line = get_next_line(fd)) != NULL)
-	{
-		printf("出力：%s", one_line);
-		fflush(stdout);
-		free(one_line); //malloc使ってないからfreeはコメントアウト
-	}
-	printf("close");
-	close(fd);
-	return (0);
-}
-
-
-// int	main()
+// int	main(void)
 // {
+// 	int		fd;
+// 	char	*one_line;
 
-// 	int  fd;
-	// char buf[100];
-	// char buf_n[100];
-	// char static	memo[100];
-	// int byte_num;
-	// char *newline_pos;
-
-
-	// byte_num = 0;
-	//  memset(buf, '\0', 100);
-	//  memset(buf_n, '\0', 100);
-	//  	memset(memo, '\0', 100);
-
-	// fd = open("./text.txt", O_RDONLY);
-	// if(fd == -1)
-    // 	{
-    //     	fprintf(stdout, "ファイルオープンエラー\n");
-    //    		 return 1;
-   	//  }
-	// for (size_t i = 0; i < 3; i++)
-	// {
-	// 	printf ("%s", get_next_line(fd));
-	// }
-
-	// int i = 0;
-	// while (buf[i] != '\n')
-	// {
-	// 	 byte_num = read(fd, &buf, BUFFER_SIZE);
-	//    if(byte_num == -1)
-	// 	{
-	// 		fprintf(stdout, "ファイル読み込みエラー\n");
-	// 		return 1;
-	// 	}
-	// 	i++;
-	// }
-	// printf("fileから%dバイト読み込みました。\n", byte_num);
-	// // 改行文字を探す
-	// newline_pos = ft_strchr(buf, '\n');
-	// if (newline_pos != NULL)
-	// {
-	// 	// 改行文字までの部分を buf_n にコピー
-	// 	strncpy(buf_n, buf, newline_pos - buf);
-
-	// 	// 改行文字以降を memo にコピー
-	// 	strcpy(memo, newline_pos + 1);
-	// }
-
-	// 	fprintf(stdout, "読み込んだ文字列:%s\n", buf);
-	// fprintf(stdout, "改行までの文字列:%s\n", buf_n);
-	// fprintf(stdout, "改行以降の文字列:%s\n", memo);
-
+// 	fd = open("./text.txt", O_RDONLY);
+// 	if (fd == -1)
+// 	{
+// 		perror("open");
+// 		return (1);
+// 	}
+// 	while ((one_line = get_next_line(fd)) != NULL)
+// 	{
+// 		printf("出力：%s", one_line);
+// 		fflush(stdout);
+// 		free(one_line);
+// 	}
+// 	printf("close");
 // 	close(fd);
-
-//  return (0);
-// }
-
-// int main() {
-//     int fd;
-//     char *line;
-
-//     // ファイルを開く
-//     fd = open("./text.txt", O_RDONLY);
-//     if (fd == -1) {
-//         perror("open");
-//         return (1);
-//     }
-//     // while ((line = get_next_line(fd)) != NULL) {
-//     //     printf("%s", line);
-// 	// 	fflush(stdout);
-//     //     free(line);
-//     // }
-//     close(fd);
-//     return (0);
+// 	return (0);
 // }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 20:42:33 by miyuu             #+#    #+#             */
-/*   Updated: 2024/09/07 19:00:01 by miyuu            ###   ########.fr       */
+/*   Updated: 2024/09/08 01:40:56 by miyuu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,13 @@
 #  define BUFFER_SIZE 42
 # endif
 
-#include <fcntl.h>
-
-#include <fcntl.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdbool.h>
-#include <stdint.h>//SIZE_MAX→#include <limits.h>じゃだめ
+# include <fcntl.h>
+# include <fcntl.h>
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdbool.h>
+# include <stdint.h>//SIZE_MAX→#include <limits.h>じゃだめ
 
 char	*get_next_line(int fd);
 size_t	ft_strlen(const char *s);
